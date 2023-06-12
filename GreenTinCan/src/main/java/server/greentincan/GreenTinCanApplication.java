493b45d9a4d1da6670dff1eb3a804853fff2152c
chosunghyun18 <krismon@naver.com>
2023-06-12 13:53:13 +0900

    chore : init project settings -set git ignore

---
+++ GreenTinCan/src/main/java/server/greentincan/GreenTinCanApplication.java
@@ -0,0 +1,13 @@
+package server.greentincan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenTinCanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenTinCanApplication.class, args);
+    }
+
+}
---
+++ GreenTinCan/src/test/java/server/greentincan/GreenTinCanApplicationTests.java
@@ -0,0 +1,13 @@
+package server.greentincan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenTinCanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
