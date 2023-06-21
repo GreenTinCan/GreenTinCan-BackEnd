566c59047174f0ee90f39aa6a44fdd25eb4f426d
chosunghyun18 <krismon@naver.com>
2023-06-21 19:37:40 +0900

    feat: add comm  api response in sendMessage

--- ProTalkTime/src/main/java/server/protalktime/chat/controller/ChatApiController.java
+++ ProTalkTime/src/main/java/server/protalktime/chat/controller/ChatApiController.java
@@ -5,19 +5,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import server.protalktime.chat.domain.application.ChatService;
 import server.protalktime.chat.domain.model.ChatMessage;
 
 @Slf4j
 @RequiredArgsConstructor
 @Controller
 public class ChatApiController {
-    private final SimpMessageSendingOperations messagingTemplate;
 
+    private final ChatService chatService;
     @MessageMapping("/chat/message")
     public void message(ChatMessage message) {
         if (ChatMessage.MessageType.ENTER.equals(message.getType()))
             message.setMessage(message.getSender() + "님이 입장하셨습니다.");
-        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+        chatService.convertAndSend(message);
     }
+
 }
--- ProTalkTime/src/main/java/server/protalktime/chat/domain/application/ChatService.java
+++ ProTalkTime/src/main/java/server/protalktime/chat/domain/application/ChatService.java
@@ -1,5 +1,17 @@
 package server.protalktime.chat.domain.application;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Service;
+import server.protalktime.chat.domain.model.ChatMessage;
+
+@Service
+@RequiredArgsConstructor
 public class ChatService {
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    public void convertAndSend(ChatMessage message) {
+        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+    }
 
 }
