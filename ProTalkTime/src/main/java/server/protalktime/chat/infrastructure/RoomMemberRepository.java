a015a31b6a74752e162e8ec8381a6309b585fa29
chosunghyun18 <krismon@naver.com>
2023-06-22 23:41:11 +0900

    feat : add join chat room api

--- ProTalkTime/src/main/java/server/protalktime/chat/infrastructure/RoomMemberRepository.java
+++ ProTalkTime/src/main/java/server/protalktime/chat/infrastructure/RoomMemberRepository.java
@@ -1,12 +1,21 @@
 package server.protalktime.chat.infrastructure;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import server.protalktime.chat.domain.model.Room;
 import server.protalktime.chat.domain.model.RoomMember;
+import server.protalktime.member.domain.model.Member;
 
 @Repository
 public interface RoomMemberRepository extends JpaRepository<RoomMember,Long> {
     List<RoomMember> findAllByMemberId(Long memberId);
     List<RoomMember> findAllByRoomId(Long roomId);
+
+
+    @Query("SELECT rm FROM RoomMember rm WHERE rm.member.id = :memberId AND rm.room.id = :roomId")
+    Optional<RoomMember> findByMemberIdAndRoomId(@Param("memberId") Long memberId, @Param("roomId") Long roomId);
 }
