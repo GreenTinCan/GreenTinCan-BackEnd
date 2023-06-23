641afb1dc9559158f89463812f4a3184abf797f8
chosunghyun18 <krismon@naver.com>
2023-06-23 18:34:46 +0900

    feat : add

--- ProTalkTime/src/main/java/server/protalktime/chat/domain/application/ChatService.java
+++ ProTalkTime/src/main/java/server/protalktime/chat/domain/application/ChatService.java
@@ -3,15 +3,35 @@ package server.protalktime.chat.domain.application;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
+import server.protalktime.chat.domain.model.ChatEntity;
 import server.protalktime.chat.domain.model.ChatMessage;
+import server.protalktime.chat.dto.ChatDtos.*;
+import server.protalktime.chat.infrastructure.ChatEntityRepository;
+import server.protalktime.member.domain.application.MemberService;
 
 @Service
 @RequiredArgsConstructor
 public class ChatService {
     private final SimpMessageSendingOperations messagingTemplate;
+    private final RoomService roomService;
+    private final MemberService memberService;
 
     public void convertAndSend(ChatMessage message) {
         messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
     }
+
+    // sector for hackaton only
+    private final ChatEntityRepository chatEntityRepository;
+    public ChatResponseDto sendAndSaveMessage(Long memberId ,Long roomId,ChatRequestDto requestDto) {
+        memberService.findByMemberId(memberId);
+        roomService.findRoomById(roomId);
+        ChatEntity chatEntity = new ChatEntity(
+            memberService.findByMemberId(memberId),
+            roomService.findRoomById(roomId),
+            requestDto);
+        chatEntityRepository.save(chatEntity);
+        return new ChatResponseDto(chatEntity);
+    }
+
 
 }
--- ProTalkTime/src/main/java/server/protalktime/chat/dto/ChatDtos.java
+++ ProTalkTime/src/main/java/server/protalktime/chat/dto/ChatDtos.java
@@ -1,5 +1,47 @@
 package server.protalktime.chat.dto;
 
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import server.protalktime.chat.domain.model.ChatEntity;
+import server.protalktime.chat.domain.model.ChatEntity.MessageType;
+
+
+@Data
 public class ChatDtos {
+    @Data
+    public static class ChatResponseDto {
+        private Long roomId;
+
+        private Long chatMessageId;
+        private MessageType type;
+        private String message;
+        private LocalDateTime messageTime;
+
+        private Long memberId;
+        private String memberNickName;
+        private String memberSex;
+        private String memberImageUrl;
+        private String memberCareer;
+        public ChatResponseDto(ChatEntity chatEntity) {
+            this.roomId = chatEntity.getRoom().getId();
+            this.chatMessageId = chatEntity.getId();
+            this.type = chatEntity.getType();
+            this.message = chatEntity.getMessage();
+            this.messageTime =chatEntity.getCreatedDateTime();
+
+            this.memberId = chatEntity.getMember().getId();
+            this.memberNickName = chatEntity.getMember().getNickName();
+            this.memberSex = chatEntity.getMember().getSex();
+            this.memberImageUrl = chatEntity.getMember().getProfileUrl();
+            this.memberCareer = chatEntity.getMember().getCareer();
+        }
+    }
+    @Data
+    @NoArgsConstructor
+    public static class ChatRequestDto {
+        private MessageType type;
+        private String message;
+    }
 
 }
